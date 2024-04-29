82d24783dec5188a68114d183c4a8bb7e3450444
RicardoAzevedo <ricardoazevedo1911@gmail.com>
2024-04-29 15:45:29 -0300

    Exemplo simples de como utilizar a indentação

---
+++ src/main/java/com/ricardooazevedo/java/anatomia/classe/BoletimEstudantil.java
@@ -0,0 +1,23 @@
+
+package com.ricardooazevedo.java.anatomia.classe;
+
+/**
+ *
+ * @author ricar
+ */
+public class BoletimEstudantil {
+    //Exemplo sobre como utilizar a indentação:
+    public static void main(String[] args){
+        
+        int mediaFinal = 6;
+        
+        if(mediaFinal < 6){
+            System.out.println("Reprovado!");
+        }else if(mediaFinal == 6){
+            System.out.println("Prova Minerva!");
+        }else{
+            System.out.println("Aprovado!");
+        }
+        
+    }
+}
