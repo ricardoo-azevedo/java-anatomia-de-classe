015c6041f12e3638eef22858daae3b64f0884f57
RicardoAzevedo <ricardoazevedo1911@gmail.com>
2024-04-29 15:32:07 -0300

    Exemplo de uso sobre a declaração de metodos e variaveis

--- src/main/java/com/ricardooazevedo/java/anatomia/classe/JavaAnatomiaClasse.java
+++ src/main/java/com/ricardooazevedo/java/anatomia/classe/JavaAnatomiaClasse.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 package com.ricardooazevedo.java.anatomia.classe;
 
 /**
@@ -11,19 +9,19 @@ public class JavaAnatomiaClasse {
 
     //revisão sobre a sintaxe basica do java:
     public static void main(String[] args) {
-
-        System.out.println("Ola turma, sejam bem-vindos!");
-
-        //Exemplo de declaração de variaveis:
-        String meuNome = "Ricardo";
-
-        int anoFabricacao = 2024;
-
-        boolean verdadeira = false;
+        //Exemplo de uso:
+        
+        String primeiroNome = "Ricardo";
+        
+        String segundoNome = "Azevedo";
+        
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        
+        System.out.println(nomeCompleto);
 
     }
 
-    //Exemplo de declaração de métodos:
+    
     public static String nomeCompleto(String primeiroNome, String segundoNome) {
         return primeiroNome.concat(" ").concat(segundoNome);
     }
